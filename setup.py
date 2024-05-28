d3d03b0fa5359443857f33efc499a526c9596269
Nabuki <shichiromail@gmail.com>
2024-05-28 22:03:47 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="persistent_queue",
+    version="1.0",
+    description="A standalone module for persistent_queue.py",
+    author="Vitaly Mahonin",
+    author_email="nabuki@vk.com",
+    py_modules=["persistent_queue"],
+)
