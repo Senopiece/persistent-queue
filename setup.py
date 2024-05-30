99d7bf1e7d579ca0b53ac07562bfd173df87d9b2
Nabuki <shichiromail@gmail.com>
2024-05-30 17:08:43 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="persistent_queue",
-    version="1.2",
+    version="1.3",
     description="A standalone module for persistent queue",
     author="Vitaly Mahonin",
     author_email="nabuki@vk.com",
